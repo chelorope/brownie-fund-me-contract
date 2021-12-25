b669a31a08adac6a6c16b94e362e3d92cc4cb2cc
chelorope <cmarcelorp@gmail.com>
2021-12-25 16:55:13 -0300

    mainnet fork

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,12 +1,13 @@
 from brownie import MockV3Aggregator, network, config, accounts
 from web3 import Web3
 
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 DECIMALS = 8
 STARTING_VALUE = 200000000
 
 def get_account():
-  if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+  if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
     return accounts[0]
   else:
     return accounts.add(config["wallets"]["from_key"])
